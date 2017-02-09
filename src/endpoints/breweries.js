3c8b5241738ffcad4e4a8700a30130119631c994
Gramatiik <stanyslas.bres@gmail.com>
2017-02-09 23:46:13 +0100

    WIP : apidoc

--- src/endpoints/breweries.js
+++ src/endpoints/breweries.js
@@ -3,6 +3,13 @@ import RequestBuilder from "../helpers/RequestBuilder";
 
 export default function breweriesEndpoints(server) {
 
+    /**
+     * @api {get} /breweries/count get the count of breweries
+     * @apiName GetBreweriesCount
+     * @apiGroup Breweries
+     * @apiVersion 0.1.0
+     * @apiSuccess {Number} count number of breweries
+     */
     server.get('/breweries/count', (req, res, next) => {
         db.Brewery.count().then( function(count) {
             console.log(count);
@@ -13,6 +20,112 @@ export default function breweriesEndpoints(server) {
         });
     });
 
+    /**
+     * @api {get} /breweries get a list of breweries
+     * @apiName GetBrewery
+     * @apiGroup Breweries
+     * @apiVersion 0.1.0
+     * @apiSuccess {Object[]} Brewery Brewery response object
+     * @apiSuccess {Number}     Brewery.id id of the record
+     * @apiSuccess {String}     Brewery.name Name of this brewery
+     * @apiSuccess {String}     Brewery.address1 First address line
+     * @apiSuccess {String}     Brewery.address2 Second address line
+     * @apiSuccess {String}     Brewery.city City
+     * @apiSuccess {String}     Brewery.state State (if applicable)
+     * @apiSuccess {String}     Brewery.code ZIP code
+     * @apiSuccess {String}     Brewery.country Country
+     * @apiSuccess {String}     Brewery.phone Phone
+     * @apiSuccess {String}     Brewery.website Website
+     * @apiSuccess {String}     Brewery.descript Description
+     * @apiSuccess {Object}     Brewery.BreweryGeocode Geocode related object (if present)
+     * @apiSuccess {Object[]}     Brewery.Beers Beers that belong to this brewery
+     */
+    server.get({
+        url: '/breweries',
+        validation: {
+            queries: {
+                fields: { isRequired: false, regex: /^(([a-zA-Z0-9\-_],*)+|\*)$/, descriprion: "Fields to include in response (comma separated)"},
+                recursive: { isRequired: false, regex: /^(true|false)$/},
+                limit: { isRequired: false, isNumeric: true },
+                offset: { isRequired: false, isNumeric: true },
+                order: { isRequired: false, regex: /(^\w+:\w+$)/, descriprion: "<field>:<ordering>"}
+            }
+        }
+    }, (req, res, next) => {
+
+        let query = new RequestBuilder(req, db, 'Brewery', {
+            include: [db.BreweryGeocode, db.Beer ],
+            defaultLimit: 20,
+            maxLimit: 50
+        })  .enableFieldsSelection()
+            .enableRecursivity()
+            .enablePagination()
+            .enableOrdering()
+            .finalize();
+
+        db.Brewery.findAll(query).then( (breweries) => {
+            if (!breweries) {
+                res.send(new restify.NotFoundError("No breweries were found..."));
+            } else {
+                res.send(breweries);
+            }
+            return next();
+        }).catch((err) => {
+            return next(err);
+        });
+
+    });
+
+    /**
+     * @api {post} /breweries add a new brewery
+     * @apiName PostBrewery
+     * @apiGroup Breweries
+     * @apiVersion 0.1.0
+     */
+    server.post({
+        url: '/breweries',
+        validation: {
+            content: {
+                name: { isRequired: true },
+                address1: { isRequired: true, isAlphanumeric: true },
+                address2: { isRequired: false, isAlphanumeric: true },
+                city: { isRequired: true, isAlphanumeric: true },
+                state: { isRequired: false, isAlphanumeric: true },
+                code: { isRequired: false, isAlphanumeric: true },
+                country: { isRequired: true, isAlphanumeric: true },
+                phone: { isRequired: false, isAlphanumeric: true },
+                website: { isRequired: false, isAlphanumeric: true },
+                descript: { isRequired: false, isAlphanumeric: true },
+            }
+        }
+    }, (req, res, next) => {
+
+        let newBrewery = db.Brewery.build(req.body);
+
+        res.send(newBrewery)
+
+        return next();
+    });
+
+    /**
+     * @api {get} /breweries/:id get a brewery informations by id
+     * @apiName GetBreweryId
+     * @apiGroup Breweries
+     * @apiVersion 0.1.0
+     *
+     * @apiSuccess {Object} Brewery Brewery response object
+     * @apiSuccess {Number}     Brewery.id id of the record
+     * @apiSuccess {Number}     Brewery.name Name of this brewery
+     * @apiSuccess {Number}     Brewery.address1 First address line
+     * @apiSuccess {Number}     Brewery.address2 Second address line
+     * @apiSuccess {Number}     Brewery.city City
+     * @apiSuccess {String}     Brewery.state State (if applicable)
+     * @apiSuccess {String}     Brewery.code ZIP code
+     * @apiSuccess {String}     Brewery.country Country
+     * @apiSuccess {String}     Brewery.phone Phone
+     * @apiSuccess {String}     Brewery.website Website
+     * @apiSuccess {String}     Brewery.descript Description
+     */
     server.get({
         url: '/breweries/:id',
         validation: {
@@ -48,64 +161,4 @@ export default function breweriesEndpoints(server) {
 
     });
 
-    server.get({
-        url: '/breweries',
-        validation: {
-            queries: {
-                fields: { isRequired: false, regex: /^(([a-zA-Z0-9\-_],*)+|\*)$/, descriprion: "Fields to include in response (comma separated)"},
-                recursive: { isRequired: false, regex: /^(true|false)$/},
-                limit: { isRequired: false, isNumeric: true },
-                offset: { isRequired: false, isNumeric: true },
-                order: { isRequired: false, regex: /(^\w+:\w+$)/, descriprion: "<field>:<ordering>"}
-            }
-        }
-    }, (req, res, next) => {
-
-        let query = new RequestBuilder(req, db, 'Brewery', {
-            include: [db.BreweryGeocode, db.Beer ],
-            defaultLimit: 20, //number of objects to fetch if no limit specified
-            maxLimit: 50 //maximum number of objects to fetch if limit was specified in query
-        })  .enableFieldsSelection()
-            .enableRecursivity()
-            .enablePagination()
-            .enableOrdering()
-            .finalize();
-
-        db.Brewery.findAll(query).then( (breweries) => {
-            if (!breweries) {
-                res.send(new restify.NotFoundError("No breweries were found..."));
-            } else {
-                res.send(breweries);
-            }
-            return next();
-        }).catch((err) => {
-            return next(err);
-        });
-
-    });
-
-    server.post({
-        url: '/breweries',
-        validation: {
-            content: {
-                name: { isRequired: true },
-                address1: { isRequired: true, isAlphanumeric: true },
-                address2: { isRequired: false, isAlphanumeric: true },
-                city: { isRequired: true, isAlphanumeric: true },
-                state: { isRequired: false, isAlphanumeric: true },
-                code: { isRequired: false, isAlphanumeric: true },
-                country: { isRequired: true, isAlphanumeric: true },
-                phone: { isRequired: false, isAlphanumeric: true },
-                website: { isRequired: false, isAlphanumeric: true },
-                descript: { isRequired: false, isAlphanumeric: true },
-            }
-        }
-    }, (req, res, next) => {
-
-        let newBrewery = db.Brewery.build(req.body);
-
-        res.send(newBrewery)
-
-        return next();
-    });
 }
